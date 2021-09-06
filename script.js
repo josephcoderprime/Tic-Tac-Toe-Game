bf4a7f8133b26045a1eab126930b6b72f143a3b0
josephcoderprime <77362650+josephcoderprime@users.noreply.github.com>
2021-09-06 21:17:52 +0100

    Select box modifications

---
+++ script.js
@@ -0,0 +1,9 @@
+//selecting all required elements 
+
+const selectBox = dodument.querySelector(".select-box"),
+SelectXBtn = selectBox.querySelector(".playerX"),
+SelectOBtn = selectBox.querySelector(".playerO"); 
+
+wondow.onload = () => { //once window loaded 
+
+}
